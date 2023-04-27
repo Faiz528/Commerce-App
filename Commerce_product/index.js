8e27bb66b24b179d0716559e063b19d442e8d55d
MD FAIZUDDIN <84907848+Faiz528@users.noreply.github.com>
2023-04-27 22:09:35 +0530

    Bugs fixed

--- Commerce_product/index.js
+++ Commerce_product/index.js
@@ -1,3 +1,4 @@
+var total=0;
 function save(event){
     event.preventDefault()
     var price = event.target.amount.value;
@@ -8,63 +9,71 @@ function save(event){
         item,
     }
 
-    axios.post("https://crudcrud.com/api/58910834ede14bea83cd99752649e1e0/cart",object)
+    axios.post("https://crudcrud.com/api/8b68405903bb419da8de6c29f0c4b039/cart",object)
     .then((response)=>{
         show(response.data)
         console.log(response);
+
+            total = total + parseInt(response.data.price)
+            console.log(total);
+            document.getElementById('H2').innerText = `Total value worth of products is Rs: ${total}`;
+
     })
     .catch((err)=>{
         console.log(err);
     })
 }
-var total=0;
+
     window.document.addEventListener("DOMContentLoaded",()=>{
         
-        axios.get("https://crudcrud.com/api/58910834ede14bea83cd99752649e1e0/cart")
+        axios.get("https://crudcrud.com/api/8b68405903bb419da8de6c29f0c4b039/cart")
         .then((response)=>{
             console.log(response)
-            for(var j=0;j<response.data.length;j++){
-                total = total + parseInt(response.data[j].price)
-            }
-            document.getElementById('H2').insertAdjacentText("afterend", total);
-            console.log(total)
             for(var i=0;i<response.data.length;i++){
                 show(response.data[i])
+
+                total = total + parseInt(response.data[i].price)
             }
+            document.getElementById('H2').innerText = `Total value worth of products is Rs: ${total}`;
         })
         .catch((error)=>{
             console.log(error)
         })
         
-        document.getElementById('H2').insertAdjacentText("afterend", total);
     })
+
     
 
 
-function show(object){
-    var parent= document.getElementById('listofproducts');
-    var child = document.createElement('li');
-    child.textContent=(object.price+"-"+object.item+"-")
-    parent.appendChild(child)
-
-    const delbutton =document.createElement('input')
-    delbutton.type='button'
-    delbutton.value='Delete Product'
-delbutton.className='deletebutton'
-delbutton.style.border='1px solid red'
-
-    child.appendChild(delbutton)
-    delbutton.onclick = ()=>{
-        parent.removeChild(child)
-        axios.delete(`https://crudcrud.com/api/58910834ede14bea83cd99752649e1e0/cart/${object._id}`)
-                .then((response) => {
-                  console.log(response)
-                  
-                
-                })
-                .catch((error) => {
-                  console.log(error)
-                })
-      }
+    function show(object){
+        var parent = document.getElementById('listofproducts');
+        var child = document.createElement('li');
+        child.textContent = (object.price + "-" + object.item + "-");
+        parent.appendChild(child);
       
-}
+        const delbutton = document.createElement('input');
+        delbutton.type = 'button';
+        delbutton.value = 'Delete Product';
+        delbutton.className = 'deletebutton';
+        delbutton.style.border = '1px solid red';
+        child.appendChild(delbutton);
+      
+        delbutton.onclick = () => {
+            parent.removeChild(child);
+              
+          
+          axios.delete(`https://crudcrud.com/api/8b68405903bb419da8de6c29f0c4b039/cart/${object._id}`)
+            .then((response) => {
+              console.log(response);
+              //parent.removeChild(child);
+              console.log(response.data.price);
+              total = total -(object.price);
+               console.log(total)
+              document.getElementById('H2').innerText = `Total value worth of products is Rs: ${total}`;
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+    }
+
